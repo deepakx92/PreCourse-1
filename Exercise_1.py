4e8251a18d21f40f8a5bc71de79951cddfa91fe7
deepakx92 <deepakjotsingh192@gmail.com>
2025-05-18 13:09:04 -0700

    Update Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,23 +1,50 @@
+# Exercise_1 : Implement Stack using Array.
+
+# Time Complexity : O(n)
+# Space Complexity : O(n)
+# Did this code successfully run on Leetcode : yes 
+# Any problem you faced while coding this : looking on how to remove the last element of the array without using pop funciton 
+# Your code here along with comments explaining your approach -  
+
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
+    
+    # initialised the array 
+    def __init__(self):
+        self.arr = []
+
+    # returned boolean if the stack is empty or not
+    def isEmpty(self):
+        return len(self.arr) == 0
+    
+    # adding an element at the end of the stack 
+    def push(self, item):
+        self.arr.append(item)
+
+    # removing and returning the last element of the array 
+    def pop(self):
+        if self.isEmpty():
+            return None 
         
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+        s = self.size()
+        b = self.arr[s - 1]
+        self.arr = self.arr[:s-1]
+        return b
+
+    # returns the last element of array 
+    def peek(self):
+        return self.arr[-1]  
+
+    # return the size of the stack 
+    def size(self):
+        return len(self.arr)
+
+    # prints the stack 
+    def show(self):
+        return self.arr
+
 
 s = myStack()
+print(s.isEmpty())   
 s.push('1')
 s.push('2')
 print(s.pop())
